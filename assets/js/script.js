bdcb624eb8a765a3f499b0aa9ec470b8be5685e2
barrymoore1990 <hello@touringhighlights.com>
2023-02-02 15:16:05 +0000

    added birthday date form and javascript to get date on submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
 fetch("https://api.themoviedb.org/3/movie/550?api_key=7d8c2f046707227f388808b36dce76d5")
 .then(response => response.json())
 .then(data => console.log(data))
+
+let birthdayEl = document.querySelector("#birthday");
+let submitButtonEl = document.querySelector("#birthdaySubmit");
+
+submitButtonEl.addEventListener("click", function(event){
+    event.preventDefault();
+    let birthdayDate = birthdayEl.value;
+    console.log(birthdayDate);
+});
+
